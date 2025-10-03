2569b04dc932070073f6ed6058a2ddf7c9c8d4e9
Eslam Hagras <eslam.crave1996@gmail.com>
2025-10-03 11:20:16 +0300

    apple

---
+++ Apple/Script.js
@@ -0,0 +1,11 @@
+let mainPhoto = document.getElementById("mainPhoto");
+let container = document.querySelector('.container');
+
+function phones(phone){
+    mainPhoto.src = phone;
+
+}
+function colors (color){
+    container.style.background = color;
+}
+
