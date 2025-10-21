9b16f841fbc6621862342371e02c7acdaed556fa
Eslam Hagras <eslam.crave1996@gmail.com>
2025-10-21 15:14:57 +0300

    fip

---
+++ FIP/script.js
@@ -0,0 +1,103 @@
+const playerRank =document.getElementById('playerRank');
+const playerName =document.getElementById('playerName');
+const countryFlag =document.getElementById('countryFlag');
+const countyName =document.getElementById('countyName');
+const playerPoints =document.getElementById('playerPoints');
+const playerImg =document.getElementById('playerImg');
+const btn1 =document.getElementById('btn1');
+
+
+
+
+btn1.onclick= function (){
+    playerImg.src= 'meida/Alejandro-Galan.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="1";
+    playerName.textContent="Alejandro Galan";
+    countyName.textContent="ESP";
+    playerPoints.textContent="19400";
+    this.style.cssText += "border-bottom: 1px solid black";
+}
+btn2.onclick= function (){
+    playerImg.src= 'meida/Federico-Chingotto-2.webp';
+    countryFlag.src="meida/Argentina_Fip.webp";
+    playerRank.textContent="2";
+    playerName.textContent="Federico Chingotto";
+    countyName.textContent="ARG";
+    playerPoints.textContent="19400";
+    this.style.cssText += "border-bottom: 1px solid black";
+}
+btn3.onclick= function (){
+    playerImg.src= 'meida/Agustin-Tapia-2.webp';
+    countryFlag.src="meida/Argentina_Fip.webp";
+    playerRank.textContent="3";
+    playerName.textContent="Agustin Tapia";
+    countyName.textContent="ARG";
+    playerPoints.textContent="13570";
+}
+btn4.onclick= function (){
+    playerImg.src= 'meida/Arturo-Coello-2.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="4";
+    playerName.textContent="Arturo Coello";
+    countyName.textContent="ESP";
+    playerPoints.textContent="13570";
+}
+btn5.onclick= function (){
+    playerImg.src= 'meida/Stupaczuk-1.webp';
+    countryFlag.src="meida/Argentina_Fip.webp";
+    playerRank.textContent="5";
+    playerName.textContent="Franco Stupaczuk";
+    countyName.textContent="ARG";
+    playerPoints.textContent="8930";
+}
+btn6.onclick= function (){
+    playerImg.src= 'meida/13-YANGUAS.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="6";
+    playerName.textContent="Miguel Yanguas";
+    countyName.textContent="ESP";
+    playerPoints.textContent="7947";
+}
+btn7.onclick= function (){
+    playerImg.src= 'meida/Lebron-3.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="7";
+    playerName.textContent="Juan Lebron";
+    countyName.textContent="ESP";
+    playerPoints.textContent="7860";
+}
+btn8.onclick= function (){
+    playerImg.src= 'meida/Coki-Nieto.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="8";
+    playerName.textContent="Jorge Nieto";
+    countyName.textContent="ESP";
+    playerPoints.textContent="7485";
+}
+btn9.onclick= function (){
+    playerImg.src= 'meida/Martin-Di-Nenno-2.webp';
+    countryFlag.src="meida/Argentina_Fip.webp";
+    playerRank.textContent="9";
+    playerName.textContent="Martin Di Nenno";
+    countyName.textContent="ARG";
+    playerPoints.textContent="5520";
+}
+btn10.onclick= function (){
+    playerImg.src= 'meida/Sanz-3.webp';
+    countryFlag.src="meida/Spain_Fip.webp";
+    playerRank.textContent="10";
+    playerName.textContent="Jon Sanz";
+    countyName.textContent="ESP";
+    playerPoints.textContent="5235";
+}
+
+
+
+
+
+
+
+
+
+
